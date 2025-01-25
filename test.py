f7433a3ed20eaf718bc62a922bec9dc178858671
saru.s <sssssaru0987654321@gmail.com>
2025-01-25 22:19:20 +0530

    updated

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
-import time
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=2)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("/home/saru/Desktop/saru/model/converted_keras/keras_model.h5" , "/home/saru/Desktop/saru/model/converted_keras/labels.txt")
 offset = 20
 imgSize = 300
 counter = 0
 
-folder = "/home/saru/Desktop/saru/SLTM/Data/Hello"
+labels = ["Hello","how are","I love you","Thank you","yes","you"]
+
 
 while True:
     success, img = cap.read()
+    imgOutput = img.copy()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
@@ -33,6 +36,8 @@ while True:
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize-wCal)/2)
             imgWhite[:, wGap: wCal + wGap] = imgResize
+            prediction , index = classifier.getPrediction(imgWhite, draw= False)
+            print(prediction, index)
 
         else:
             k = imgSize / w
@@ -41,13 +46,17 @@ while True:
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize - hCal) / 2)
             imgWhite[hGap: hCal + hGap, :] = imgResize
+            prediction , index = classifier.getPrediction(imgWhite, draw= False)
+
+       
+        cv2.rectangle(imgOutput,(x-offset,y-offset-70),(x -offset+400, y - offset+60-50),(0,255,0),cv2.FILLED)  
+
+        cv2.putText(imgOutput,labels[index],(x,y-30),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,0),2) 
+        cv2.rectangle(imgOutput,(x-offset,y-offset),(x + w + offset, y+h + offset),(0,255,0),4)   
 
         cv2.imshow('ImageCrop', imgCrop)
         cv2.imshow('ImageWhite', imgWhite)
 
-    cv2.imshow('Image', img)
-    key = cv2.waitKey(1)
-    if key == ord("s"):
-        counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
-        print(counter)
+    cv2.imshow('Image', imgOutput)
+    cv2.waitKey(1)
+    
